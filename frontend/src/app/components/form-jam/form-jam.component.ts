ad97335e4f8070880852c0788212867b04ff3aee
Brandon Retana <branrecha140356@gmail.com>
2023-05-08 23:58:00 -0600

    Table jam and singup and singin

--- frontend/src/app/components/form-jam/form-jam.component.ts
+++ frontend/src/app/components/form-jam/form-jam.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { JamService } from 'src/app/services/jam.service';
 
 @Component({
   selector: 'app-form-jam',
@@ -9,23 +10,36 @@ import { ReactiveFormsModule } from '@angular/forms';
   
 })
 export class FormJamComponent {
-  constructor(private formBuilder:FormBuilder) { }
-
-  categories:string[] = []
+  constructor(private formBuilder:FormBuilder, private jamService: JamService) { }
+  public categories:string[] = []
   public experiences:string[] = []
 
 
   public formJam = this.formBuilder.group({
     id: [''],
-    categories: ['', Validators.required],
     name: ['', Validators.required],
     startDateTime: ['', Validators.required],
     endDateTime: ['', Validators.required],
     theme: ['', Validators.required],
+    categorie: ['', Validators.required],
   });
 
 
   public setDropdownExperienceValue(value:string){
     this.experiences.push(value)
   }
+
+  public onToggle(categorie:string) {
+    this.categories.push(categorie)
+    this.formJam.controls['categorie'].setValue(this.categories[0])
+  }
+
+  public onSubmit() {
+    console.log(this.formJam.value)
+    this.jamService.createJam(this.formJam.value).subscribe((data) => {
+      console.log(data)
+    })
+  }
+
+
 }
