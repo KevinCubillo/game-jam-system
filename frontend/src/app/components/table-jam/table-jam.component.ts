aaafd2de50048ba56f2072940980f2e4fc4b4a07
Brandon Retana <branrecha140356@gmail.com>
2023-05-08 23:13:39 -0600

    Table jam and singup and singin

---
+++ frontend/src/app/components/table-jam/table-jam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-jam',
+  templateUrl: './table-jam.component.html',
+  styleUrls: ['./table-jam.component.css']
+})
+export class TableJamComponent {
+
+}
