7013701f622d22b6524c0415fe54319bdf59add6
KevinCubillo <kevincubillo25@gmail.com>
2023-06-11 20:57:23 -0600

    form editar y crear site

--- frontend/src/app/components/form-site/form-site.component.ts
+++ frontend/src/app/components/form-site/form-site.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Site } from 'src/app/models/Site';
-import { SiteService } from 'src/app/services/site.service';
+import { Site } from '../../models/Site';
+import { SiteService } from '../../services/site.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -9,99 +9,68 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './form-site.component.html',
   styleUrls: ['./form-site.component.css'],
 })
-export class FormSiteComponent implements OnInit {
-  public formSite!: FormGroup;
-  
+export class FormSiteComponent {
+  site: Site = {
+    _id: '',
+    globalOrganizer: '',
+    country: '',
+    city: '',
+    judges: [],
+    localOrganizers: [],
+    mentors: []
+  };
+  judgeInput: string = '';
+  localOrganizerInput: string = '';
+  mentorInput: string = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private siteService: SiteService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private siteService: SiteService) {}
 
-  ngOnInit(): void {
-    this.formSite = this.formBuilder.group({
-      _id: [''],
-      globalOrganizer: ['', Validators.required],
-      country: ['', Validators.required],
-      city: ['', Validators.required],
-      judges: this.formBuilder.array([]),
-      localOrganizers: this.formBuilder.array([]),
-      mentors: this.formBuilder.array([]),
-    });
+  addJudge() {
+    if (this.judgeInput.trim() !== '') {
+      this.site.judges.push(this.judgeInput.trim());
+      this.judgeInput = '';
+    }
+  }
 
-    this.activatedRoute.paramMap.subscribe((params) => {
-      const id = params.get('id');
-      if (id) {
-        this.siteService.getSiteById(id).subscribe((siteData: any) => {
-          const site = siteData as Site;
-          this.formSite.patchValue(site);
-          this.setFormArrayValues(this.judges, site.judges);
-          this.setFormArrayValues(this.localOrganizers, site.localOrganizers);
-          this.setFormArrayValues(this.mentors, site.mentors);
-        });
+  addLocalOrganizer() {
+    if (this.localOrganizerInput.trim() !== '') {
+      this.site.localOrganizers.push(this.localOrganizerInput.trim());
+      this.localOrganizerInput = '';
+    }
+  }
+
+  addMentor() {
+    if (this.mentorInput.trim() !== '') {
+      this.site.mentors.push(this.mentorInput.trim());
+      this.mentorInput = '';
+    }
+  }
+
+  removeJudge(judge: string) {
+    this.site.judges = this.site.judges.filter(j => j !== judge);
+  }
+
+  removeLocalOrganizer(localOrganizer: string) {
+    this.site.localOrganizers = this.site.localOrganizers.filter(lo => lo !== localOrganizer);
+  }
+
+  removeMentor(mentor: string) {
+    this.site.mentors = this.site.mentors.filter(m => m !== mentor);
+  }
+
+  submitForm() {
+    const { _id, ...newSite } = this.site; // Destructure and exclude _id field
+    this.siteService.createSite(newSite).subscribe(
+      (response) => {
+        // Handle the response from the service if needed
+        console.log('Site created:', response);
+      },
+      (error) => {
+        // Handle the error if it occurs
+        console.error('Error creating site:', error);
       }
-    });
+    );
   }
-
-  get judges() {
-    return this.formSite.get('judges') as FormArray;
-  }
-
-  addJudge(judge: string = '') {
-    this.judges.push(this.formBuilder.control(judge));
-  }
-
-  removeJudge(index: number) {
-    this.judges.removeAt(index);
-  }
-
-  get localOrganizers() {
-    return this.formSite.get('localOrganizers') as FormArray;
-  }
-
-  addLocalOrganizer(organizer: string = '') {
-    this.localOrganizers.push(this.formBuilder.control(organizer));
-  }
-
-  removeLocalOrganizer(index: number) {
-    this.localOrganizers.removeAt(index);
-  }
-
-  get mentors() {
-    return this.formSite.get('mentors') as FormArray;
-  }
-
-  addMentor(mentor: string = '') {
-    this.mentors.push(this.formBuilder.control(mentor));
-  }
-
-  removeMentor(index: number) {
-    this.mentors.removeAt(index);
-  }
-
-  onSubmit() {
-    const siteData = this.formSite.value;
-    if (siteData._id) {
-      this.siteService.updateSite(siteData._id, siteData).subscribe((data) => {
-        console.log(data);
-      });
-    } else {
-      this.siteService.createSite(siteData).subscribe((data) => {
-        console.log(data);
-      });
-    }
-    this.router.navigate(['/sites']);
-  }
-
-  private setFormArrayValues(formArray: FormArray, values: string[]) {
-    formArray.clear();
-    if (values && values.length > 0) {
-      values.forEach((value: string) => {
-        formArray.push(this.formBuilder.control(value));
-      });
-    }
-  }
+  
+  
 }
-
